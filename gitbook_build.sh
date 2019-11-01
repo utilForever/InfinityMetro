154a08dd2546a43b616a5ee2b52303567df0ee1e
buttercrab <jaeyong0201@gmail.com>
2019-11-01 22:56:24 +0900

    Initial commit

---
+++ gitbook_build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+gitbook install && gitbook build
+
+git checkout gh-pages
+git pull origin gh-pages --rebase
+
+cp -R _book/* .
+
+git clean -fx node_modules
+git clean -fx _book
+
+git add .
