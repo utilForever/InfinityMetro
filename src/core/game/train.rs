6a5e2797d1b374d167b1f959bfd0b93829d03458
Jaeyong Sung <jaeyong0201@gmail.com>
2020-10-01 17:10:12 +0900

    removed person

--- src/core/game/train.rs
+++ src/core/game/train.rs
@@ -2,7 +2,29 @@ use crate::core::game::room::Room;
 
 pub struct Train {
     id: u32,
-    count: u8,
-    type_: u8,
-    room: Room,
+    pub count: u8,
+    pub type_: u8,
+    pub room: Room,
+    pub loc_func: fn(f32, u32) -> f32,
+}
+
+impl Train {
+    fn new(id: u32, type_: u8, loc_func: fn(f32, u32) -> f32) -> Train {
+        Train {
+            id,
+            count: 0,
+            type_,
+            room: Room::new(),
+            loc_func,
+        }
+    }
+
+    fn get_id(&self) -> u32 {
+        self.id
+    }
+}
+
+#[cfg(test)]
+mod train_test {
+    use super::*;
 }
